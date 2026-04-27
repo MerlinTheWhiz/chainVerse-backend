217e4858f72b1957085262adf6e0463d33011bfb
Ibinola <ibinola@users.noreply.github.com>
2026-04-27 15:37:46 +0000

    fix: move verification files to module folder, add booking schema (#368, #369, #370, #371)

--- src/private-tutoring-bookings/private-tutoring-bookings.module.ts
+++ src/private-tutoring-bookings/private-tutoring-bookings.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { PrivateTutoringBookingsController } from './private-tutoring-bookings.controller';
 import { PrivateTutoringBookingsService } from './private-tutoring-bookings.service';
+import {
+  PrivateTutoringBooking,
+  PrivateTutoringBookingSchema,
+} from './schemas/private-tutoring-booking.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: PrivateTutoringBooking.name,
+        schema: PrivateTutoringBookingSchema,
+      },
+    ]),
+  ],
   controllers: [PrivateTutoringBookingsController],
   providers: [PrivateTutoringBookingsService],
 })
